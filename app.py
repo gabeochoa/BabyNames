27166f517c2bd118b9f183e486453e51950c4a89
Gabriel Ochoa <gabe1118@yahoo.com>
2016-05-15 21:55:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -51,26 +51,3 @@ def simple():
 port = int(os.environ.get('PORT', 5000))
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True,port=port)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
